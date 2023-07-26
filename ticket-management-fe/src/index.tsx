6283f191f5258b8f76ed8a88ee6b53cc6ed16b18
UtkarshSethia <utkarsh.sethiya@techprescient.com>
2023-07-26 14:26:54 +0530

    resolved_comments

--- ticket-management-fe/src/index.tsx
+++ ticket-management-fe/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const defaultTheme = createTheme();
 root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={defaultTheme}>
+  <App />
+  </ThemeProvider>
+   </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
