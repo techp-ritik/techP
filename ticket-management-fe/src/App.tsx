311d26ae644834b76183145b061c4e5df2ff7838
UtkarshSethia <utkarsh.sethiya@techprescient.com>
2023-07-26 11:58:26 +0530

    SignIn_Page

--- ticket-management-fe/src/App.tsx
+++ ticket-management-fe/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MenuAppBar from "./components/Navbar";
+import SignIn from "./components/Signin";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuAppBar />
+        <Routes>
+        <Route path="/login" Component={SignIn}></Route>
+      </Routes>
     </div>
   );
 }
