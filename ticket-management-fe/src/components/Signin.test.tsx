93ebbdf6c8dba947fc85f37241882bc3f5a03ff6
UtkarshSethia <utkarsh.sethiya@techprescient.com>
2023-08-23 17:42:56 +0530

    testing_undo_changes

--- ticket-management-fe/src/components/Signin.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import SignIn from './Signin';
-
-test('render login page',()=>{
-  const component=render(<SignIn/>)
-  const child =component.getByLabelText("Sign In")
-  expect(child).toBeInTheDocument();
-})
